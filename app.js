c01056a0ab3a50fcf054a1ec453aaebd75006bbb
ZackLeonardo <maiz9088@icloud.com>
2016-10-10 17:41:40 +0800

    chang port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var MongoClient = require('mongodb').MongoClient
   , assert = require('assert');
 
 // Connection URL
-var url = 'mongodb://myMongoDB:32771/myproject';
+var url = 'mongodb://myMongoDB:27017/myproject';
 
 // Use connect method to connect to the server
 MongoClient.connect(url, function(err, db) {
